ece604dc170bd97bfc9816ea146075930e158933
Vinod Pai <vpai@vmware.com>
2017-04-20 11:27:15 -0700

    Update the copyrights in all the files

--- vmware/samples/vcenter/vm/create/CreateExhaustiveVm/CreateExhaustiveVm.cs
+++ vmware/samples/vcenter/vm/create/CreateExhaustiveVm/CreateExhaustiveVm.cs
@@ -1,7 +1,15 @@
-﻿/*
- * Copyright 2016 VMware, Inc.  All rights reserved.
+﻿/**
+ * *******************************************************
+ * Copyright VMware, Inc. 2016.  All Rights Reserved.
+ * SPDX-License-Identifier: MIT
+ * *******************************************************
+ *
+ * DISCLAIMER. THIS PROGRAM IS PROVIDED TO YOU "AS IS" WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, WHETHER ORAL OR WRITTEN,
+ * EXPRESS OR IMPLIED. THE AUTHOR SPECIFICALLY DISCLAIMS ANY IMPLIED
+ * WARRANTIES OR CONDITIONS OF MERCHANTABILITY, SATISFACTORY QUALITY,
+ * NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
  */
-
 namespace vmware.samples.vcenter.vm.create
 {
     using CommandLine;
